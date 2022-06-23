b5a2c169d14907b11f1e31129fd137506e2d1eb9
PatrickR <patrick@ruzman.net>
2022-06-23 18:50:54 +0200

    #DP-3 Add table to PotionsView Controller

--- DruidInventory/DruidInventory/PotionsTableViewController.swift
+++ DruidInventory/DruidInventory/PotionsTableViewController.swift
@@ -11,5 +11,34 @@ class PotionsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupTableView()
+    }
+}
+
+extension PotionsTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Potion") else {fatalError("Error loading cell")}
+        return cell
+    }
+
+    func setupTableView() {
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Potion")
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+
     }
 }
