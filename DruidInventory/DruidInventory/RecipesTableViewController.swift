b7045edaa2ecbea85358f1b086deea4c6cf59abe
PatrickR <patrick@ruzman.net>
2022-06-24 21:48:38 +0200

    * add recipes struct * add ingredients struct
    Resolve conflict in new files

--- DruidInventory/DruidInventory/RecipesTableViewController.swift
+++ DruidInventory/DruidInventory/RecipesTableViewController.swift
@@ -9,7 +9,22 @@ import UIKit
 
 class RecipesTableViewController: UITableViewController {
 
+    var recipes = [String]()
+    var ingredients = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        recipes.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "recipe",
+            for: indexPath) as? RecipeCell else {fatalError("Issue loading cell")}
+
+        return cell
     }
 }
