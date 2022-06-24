6a497dc78d158ded7e5bea2fc38be46e945e9ea3
PatrickR <patrick@ruzman.net>
2022-06-24 08:22:12 +0200

    #DP-3 Configure PotionDetailView layout

--- DruidInventory/DruidInventory/PotionDetailView.swift
+++ DruidInventory/DruidInventory/PotionDetailView.swift
@@ -11,7 +11,11 @@ class PotionDetailView: UIViewController {
 
     var potion: Potion
     var indexPath: IndexPath
-    var label = UITextField()
+    var nameTextField = UITextField()
+    var image = UIImageView()
+    var amountTextField = UITextField()
+    var buttonPlus = UIButton(type: .custom)
+    var buttonMinus = UIButton(type: .custom)
 
     required init (potion: Potion, indexPath: IndexPath) {
         self.potion = potion
@@ -25,11 +29,90 @@ class PotionDetailView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
-        view.addSubview(label)
-        label.text = potion.name
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 30)
-        label.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20).isActive = true
+        view.backgroundColor = .gray
+        configTextFieldLayout()
+        configImageLayout()
+        configAmountLayout()
+        configButtonsLayout(button: buttonPlus)
+        configButtonsLayout(button: buttonMinus)
+    }
+}
+
+extension PotionDetailView {
+    func configTextFieldLayout() {
+        view.addSubview(nameTextField)
+        nameTextField.text = potion.name
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        nameTextField.font = UIFont.systemFont(ofSize: 30)
+        nameTextField.layer.borderColor = UIColor.white.cgColor
+        nameTextField.layer.borderWidth = 1.5
+        nameTextField.layer.cornerRadius = 3
+        nameTextField.textAlignment = .center
+
+        NSLayoutConstraint.activate([
+            nameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nameTextField.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+
+    func configImageLayout() {
+        view.addSubview(image)
+        image.image = UIImage(systemName: potion.image)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.borderWidth = 1.5
+        image.layer.cornerRadius = 3
+
+        NSLayoutConstraint.activate([
+            image.heightAnchor.constraint(equalToConstant: 200),
+            image.widthAnchor.constraint(equalToConstant: 200),
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.bottomAnchor.constraint(equalTo: nameTextField.topAnchor, constant: -50)
+        ])
+
+    }
+
+    func configAmountLayout() {
+        view.addSubview(amountTextField)
+        amountTextField.text = String(potion.amount)
+        amountTextField.translatesAutoresizingMaskIntoConstraints = false
+        amountTextField.font = UIFont.systemFont(ofSize: 50)
+        amountTextField.layer.borderColor = UIColor.white.cgColor
+        amountTextField.layer.borderWidth = 1.5
+        amountTextField.layer.cornerRadius = 3
+        amountTextField.textAlignment = .center
+
+        NSLayoutConstraint.activate([
+            amountTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            amountTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 50),
+            amountTextField.widthAnchor.constraint(equalTo: amountTextField.heightAnchor)
+        ])
+
+    }
+
+    func configButtonsLayout(button: UIButton) {
+        view.addSubview(button)
+
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalTo: amountTextField.widthAnchor),
+            button.heightAnchor.constraint(equalTo: button.widthAnchor),
+            button.centerYAnchor.constraint(equalTo: amountTextField.centerYAnchor)
+        ])
+
+        switch button {
+        case buttonPlus:
+            button.leadingAnchor.constraint(equalTo: amountTextField.trailingAnchor, constant: 20).isActive = true
+            button.setImage(UIImage(systemName: "plus"), for: .normal)
+        case buttonMinus:
+            button.trailingAnchor.constraint(equalTo: amountTextField.leadingAnchor, constant: -20).isActive = true
+            button.setImage(UIImage(systemName: "minus"), for: .normal)
+        default:
+            button.titleLabel?.text = "default"
+        }
     }
 }
