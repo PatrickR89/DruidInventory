a9196ab56916b79379413748720a892538c0166b
PatrickR <patrick@ruzman.net>
2022-06-27 16:08:01 +0200

    Resolve conflict in creating new file RecipeLayout.swift

--- DruidInventory/DruidInventory/RecipeCell+PotionsLayout.swift
+++ DruidInventory/DruidInventory/RecipeCell+PotionsLayout.swift
@@ -13,12 +13,12 @@ extension RecipeCell {
         configPotion1Layout()
 
         NSLayoutConstraint.activate([
-            potion1Image.widthAnchor.constraint(equalToConstant: 25),
-            potion1Image.heightAnchor.constraint(equalToConstant: 25),
-            potion1Image.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            potion1Image.leadingAnchor.constraint(equalTo: resultsIn.trailingAnchor, constant: 60),
-            potion1Amount.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            potion1Amount.trailingAnchor.constraint(equalTo: potion1Image.leadingAnchor, constant: -5)
+            potions[0].image.widthAnchor.constraint(equalToConstant: 25),
+            potions[0].image.heightAnchor.constraint(equalToConstant: 25),
+            potions[0].image.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            potions[0].image.leadingAnchor.constraint(equalTo: resultsIn.trailingAnchor, constant: 60),
+            potions[0].amount.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            potions[0].amount.trailingAnchor.constraint(equalTo: potions[0].image.leadingAnchor, constant: -5)
         ])
     }
 
@@ -28,36 +28,36 @@ extension RecipeCell {
         configPotion2Layout()
 
         NSLayoutConstraint.activate([
-            potion2Image.widthAnchor.constraint(equalToConstant: 25),
-            potion2Image.heightAnchor.constraint(equalToConstant: 25),
-            potion2Image.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            potion2Image.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            potion2Amount.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            potion2Amount.trailingAnchor.constraint(equalTo: potion2Image.leadingAnchor, constant: -5),
-            potion1Image.widthAnchor.constraint(equalToConstant: 25),
-            potion1Image.heightAnchor.constraint(equalToConstant: 25),
-            potion1Image.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            potion1Image.trailingAnchor.constraint(equalTo: potion2Image.leadingAnchor, constant: -30),
-            potion1Amount.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            potion1Amount.trailingAnchor.constraint(equalTo: potion1Image.leadingAnchor, constant: -5)
+            potions[1].image.widthAnchor.constraint(equalToConstant: 25),
+            potions[1].image.heightAnchor.constraint(equalToConstant: 25),
+            potions[1].image.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            potions[1].image.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            potions[1].amount.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            potions[1].amount.trailingAnchor.constraint(equalTo: potions[1].image.leadingAnchor, constant: -5),
+            potions[0].image.widthAnchor.constraint(equalToConstant: 25),
+            potions[0].image.heightAnchor.constraint(equalToConstant: 25),
+            potions[0].image.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            potions[0].image.trailingAnchor.constraint(equalTo: potions[1].image.leadingAnchor, constant: -30),
+            potions[0].amount.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            potions[0].amount.trailingAnchor.constraint(equalTo: potions[0].image.leadingAnchor, constant: -5)
         ])
     }
 
     func configPotion1Layout() {
-        contentView.addSubview(potion1Image)
-        potion1Image.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(potions[0].image)
+        potions[0].image.translatesAutoresizingMaskIntoConstraints = false
 
-        contentView.addSubview(potion1Amount)
-        potion1Amount.translatesAutoresizingMaskIntoConstraints = false
-        potion1Amount.text = "1x"
+        contentView.addSubview(potions[0].amount)
+        potions[0].amount.translatesAutoresizingMaskIntoConstraints = false
+        potions[0].amount.text = "1x"
     }
 
     func configPotion2Layout() {
-        contentView.addSubview(potion2Image)
-        potion2Image.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(potions[1].image)
+        potions[1].image.translatesAutoresizingMaskIntoConstraints = false
 
-        contentView.addSubview(potion2Amount)
-        potion2Amount.translatesAutoresizingMaskIntoConstraints = false
-        potion2Amount.text = "1x"
+        contentView.addSubview(potions[1].amount)
+        potions[1].amount.translatesAutoresizingMaskIntoConstraints = false
+        potions[1].amount.text = "1x"
     }
 }
