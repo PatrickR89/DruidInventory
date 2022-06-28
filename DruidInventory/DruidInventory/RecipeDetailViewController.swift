380f58f63f7f199e20d7d3b11ac9317d0ea5446e
PatrickR <patrick@ruzman.net>
2022-06-28 11:27:04 +0200

    Resolve conflict in creating new file RecipeDetailViewController

---
+++ DruidInventory/DruidInventory/RecipeDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeDetailViewController.swift
+//  DruidInventory
+//
+//  Created by Patrick on 28.06.2022..
+//
+
+import UIKit
+
+class RecipeDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
