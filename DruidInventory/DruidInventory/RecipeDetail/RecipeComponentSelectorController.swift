620a00ffbd4e3dd57668d1ac7251dc3f9ebff564
PatrickR <patrick@ruzman.net>
2022-07-07 13:13:30 +0200

    #DP-6 [Add] Previous/Next buttons & amountTextField functionality
    * [Add] potions array walkthrough

--- DruidInventory/DruidInventory/RecipeDetail/RecipeComponentSelectorController.swift
+++ DruidInventory/DruidInventory/RecipeDetail/RecipeComponentSelectorController.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class RecipeComponentSelectorController: UIViewController {
 
-    var potion = Potion(name: "", image: "", amount: 0) {
+    var potionIndex = 0
+    var potion = PotionSingleton.shared.potions[0] {
         didSet {
-            amountTextField.text = String(potion.amount)
             image.image = UIImage(systemName: potion.image)
+            nameLabel.text = potion.name
         }
     }
 
     lazy var nameLabel = UILabel()
     lazy var image = UIImageView()
-    lazy var amountTextField = UILabel()
+    lazy var amountTextField = UITextField()
+    var amount = 0 {
+        didSet {
+            amountTextField.text = String(amount)
+        }
+    }
 
     lazy var buttonNext = UIButton(type: .custom)
     lazy var buttonPrevious = UIButton(type: .custom)
@@ -56,11 +62,11 @@ extension RecipeComponentSelectorController {
         case buttonNext:
             button.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: 20).isActive = true
             button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
-            button.addTarget(self, action: #selector(addOnTap), for: .touchUpInside)
+            button.addTarget(self, action: #selector(nextOnTap), for: .touchUpInside)
         case buttonPrevious:
             button.trailingAnchor.constraint(equalTo: image.leadingAnchor, constant: -20).isActive = true
             button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-            button.addTarget(self, action: #selector(removeOnTap), for: .touchUpInside)
+            button.addTarget(self, action: #selector(previousOnTap), for: .touchUpInside)
         default:
             button.titleLabel?.text = "default"
         }
@@ -88,12 +94,29 @@ extension RecipeComponentSelectorController {
         }
     }
 
-    @objc func addOnTap() {
+    @objc func nextOnTap() {
+        potionIndex += 1
+        if potionIndex > PotionSingleton.shared.potions.count - 1 {
+            potionIndex = 0
+        }
+        potion = PotionSingleton.shared.potions[potionIndex]
+    }
 
+    @objc func previousOnTap() {
+        potionIndex -= 1
+        if potionIndex < 0 {
+            potionIndex = PotionSingleton.shared.potions.count - 1
+        }
+
+        potion = PotionSingleton.shared.potions[potionIndex]
+    }
+
+    @objc func addOnTap() {
+        amount += 1
     }
 
     @objc func removeOnTap() {
-
+        amount -= 1
     }
 
     @objc func addNewOnTap() {
@@ -138,11 +161,12 @@ extension RecipeComponentSelectorController {
     func configAmountLayout() {
         view.addSubview(amountTextField)
 
-        amountTextField.text = String(potion.amount)
+        amountTextField.text = String(amount)
         amountTextField.translatesAutoresizingMaskIntoConstraints = false
         amountTextField.font = UIFont.systemFont(ofSize: 50)
         amountTextField.textAlignment = .center
         amountTextField.textColor = .black
+        amountTextField.delegate = self
 
         NSLayoutConstraint.activate([
             amountTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -167,3 +191,29 @@ extension RecipeComponentSelectorController {
     }
 }
 
+extension RecipeComponentSelectorController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+
+        if textField == self.amountTextField {
+            guard let amountText = amountTextField.text else {return}
+            guard let tempAmount = Int(amountText) else {return}
+            amount = tempAmount
+        }
+    }
+
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String) -> Bool {
+        if textField == self.amountTextField {
+            guard let currentAmount = amountTextField.text else {return false}
+            guard let stringRange = Range(range, in: currentAmount) else {return false}
+            let updatedString = currentAmount.replacingCharacters(in: stringRange, with: string)
+
+            return (
+                updatedString.count <= 2 &&
+                string.rangeOfCharacter(from: NSCharacterSet.decimalDigits.inverted) == nil)
+        }
+        return true
+    }
+}
