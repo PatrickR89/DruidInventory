29cdb7266a10c788baa218cbae45dc007711e3ff
PatrickR <patrick@ruzman.net>
2022-07-07 08:21:44 +0200

    #DP-5 [Add] collectionView and FlowLayout with constraints
    * [Add] initialize cell

--- DruidInventory/DruidInventory/Potions/ImageSelectorCell.swift
+++ DruidInventory/DruidInventory/Potions/ImageSelectorCell.swift
@@ -9,4 +9,24 @@ import UIKit
 
 class ImageSelectorCell: UICollectionViewCell {
 
+    var imageName = ""
+    var imageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configCellImage(_ inputName: String) {
+        imageName = inputName
+    }
+
+    func configImageLayout() {
+
+    }
+
 }
--- DruidInventory/DruidInventory/Potions/ImageSelectorViewController.swift
+++ DruidInventory/DruidInventory/Potions/ImageSelectorViewController.swift
@@ -11,21 +11,54 @@ class ImageSelectorViewController: UIViewController {
 
     var images = [String]()
 
+    lazy var collectionLayout: UICollectionViewFlowLayout = {
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .vertical
+        collectionLayout.estimatedItemSize.height = 100
+        collectionLayout.estimatedItemSize.width = 100
+
+        return collectionLayout
+    }()
+
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: collectionLayout)
+        return collectionView
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initializeImages()
+        configCollectionViewLayout()
+
     }
 
     func initializeImages() {
         let homeTabBar = HomeTabBarController()
         images = homeTabBar.potionSymbolNames
     }
+
+    func configCollectionViewLayout() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        collectionView.register(ImageSelectorCell.self, forCellWithReuseIdentifier: "image")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
 }
 
 extension ImageSelectorViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        images.count
     }
 
     func collectionView(
