d277cdf6ada2b88e1d3d297112c63cdb8d5e818b
PatrickR <patrick@ruzman.net>
2022-07-07 09:08:00 +0200

    #DP-5 [Add] Keyboard interactions and response
    * [Fix] DEVELOPMENT_TEAM for testing on device

--- DruidInventory/DruidInventory/Potions/PotionDetailViewController.swift
+++ DruidInventory/DruidInventory/Potions/PotionDetailViewController.swift
@@ -48,6 +48,8 @@ class PotionDetailViewController: UIViewController {
         if newPotion {
             configAddButtonLayout()
         }
+        enableKeyboardObserver()
+        hideKeyboardOnTap()
     }
 }
 
@@ -124,6 +126,47 @@ extension PotionDetailViewController: ImageSelectorDelegate {
         potion.image = image
         if !newPotion {
             PotionSingleton.shared.changePotionImage(image: image, indexPath: indexPath)
+        }
+    }
+}
+
+extension PotionDetailViewController {
+
+    func hideKeyboardOnTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboardOnTap))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboardOnTap() {
+        view.endEditing(true)
+    }
+
+    func enableKeyboardObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (
+            notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height / 2
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
         }
     }
 }
