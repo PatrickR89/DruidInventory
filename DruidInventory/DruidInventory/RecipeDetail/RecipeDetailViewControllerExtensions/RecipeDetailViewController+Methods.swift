9c7495a333ad3b4b667f1b8ee59052f292f60f05
PatrickR <patrick@ruzman.net>
2022-07-09 10:39:02 +0200

    #DP-6 [Change] refactor removeComponent function into smaller functions
    * [Change] reorder functions in extension with added markers for readability

--- DruidInventory/DruidInventory/RecipeDetail/RecipeDetailViewControllerExtensions/RecipeDetailViewController+Methods.swift
+++ DruidInventory/DruidInventory/RecipeDetail/RecipeDetailViewControllerExtensions/RecipeDetailViewController+Methods.swift
@@ -20,29 +20,6 @@ extension RecipeDetailViewController {
         RecipeDetailMakeButtonCell.register(in: tableView)
     }
 
-    func appendItemsToContent() {
-
-        for ingredient in recipe.ingredientsInRecipe {
-            tableContents.insert(
-                RecipeDetailViewController.TableRowContent.component(
-                    name: ingredient.name,
-                    image: ingredient.image,
-                    count: ingredient.amount),
-                at: 0)
-        }
-
-        if let index = tableContents.firstIndex(of: RecipeDetailViewController.TableRowContent.downArrow) {
-            for potion in recipe.potionsInRecipe {
-                tableContents.insert(
-                    RecipeDetailViewController.TableRowContent.component(
-                        name: potion.name,
-                        image: potion.image,
-                        count: potion.amount),
-                    at: index + 1)
-            }
-        }
-    }
-
     func appendPlusButtons() {
         if recipe.ingredientsInRecipe.count < 4 {
             tableContents.insert(.plusButton(type: .inputNew), at: 0)
@@ -54,21 +31,6 @@ extension RecipeDetailViewController {
             }
         }
     }
-
-    func recipeComponentManipulation(
-        type: RecipeComponentType,
-        recipe: Recipe,
-        component: Potion,
-        componentIndexPath: IndexPath) {
-
-            let recipeComponentSelector = RecipeComponentSelectorController(
-                componentIndexPath: componentIndexPath,
-                type: type, potion: component)
-            recipeComponentSelector.delegate = self
-
-            recipeComponentSelector.setupRecipe(recipe: recipe, potion: component)
-            present(recipeComponentSelector, animated: true)
-        }
 
     func validateRecipe() {
         isRecipeValid = recipe.ingredientsInRecipe.count >= 2 && recipe.potionsInRecipe.count >= 1
@@ -92,55 +54,110 @@ extension RecipeDetailViewController {
         }
     }
 
+    //MARK: tableContents & recipe components manipulation
+
+    func appendItemsToContent() {
+
+        for ingredient in recipe.ingredientsInRecipe {
+            tableContents.insert(
+                RecipeDetailViewController.TableRowContent.component(
+                    name: ingredient.name,
+                    image: ingredient.image,
+                    count: ingredient.amount),
+                at: 0)
+        }
+
+        if let index = tableContents.firstIndex(of: RecipeDetailViewController.TableRowContent.downArrow) {
+            for potion in recipe.potionsInRecipe {
+                tableContents.insert(
+                    RecipeDetailViewController.TableRowContent.component(
+                        name: potion.name,
+                        image: potion.image,
+                        count: potion.amount),
+                    at: index + 1)
+            }
+        }
+    }
+
+    func recipeComponentManipulation(
+        type: RecipeComponentType,
+        recipe: Recipe,
+        component: Potion,
+        componentIndexPath: IndexPath) {
+
+            let recipeComponentSelector = RecipeComponentSelectorController(
+                componentIndexPath: componentIndexPath,
+                type: type, potion: component)
+            recipeComponentSelector.delegate = self
+
+            recipeComponentSelector.setupRecipe(recipe: recipe, potion: component)
+            present(recipeComponentSelector, animated: true)
+        }
+
     func removeComponent(
         type: RecipeComponentType,
         component: Potion,
         recipeIndexPath: IndexPath,
         componentIndexPath: IndexPath) {
 
-        if type == RecipeComponentType.inputChange {
+            getIndex(type: type, component: component)
+
+            if let index = tableContents.firstIndex(
+                where: {$0.self == TableRowContent.component(
+                    name: component.name,
+                    image: component.image,
+                    count: component.amount)}) {
+
+                tableContents.remove(at: index)
+                tableView.deleteRows(at: [componentIndexPath], with: .none)
+            }
+
+            appendPlusButton(type: type)
+
+        }
+
+    // MARK: removeComponent internal functions
+
+    func getIndex(type: RecipeComponentType, component: Potion) {
+        switch type {
+        case .inputChange:
             if let index = recipe.ingredientsInRecipe.firstIndex(where: {$0.name == component.name}) {
                 recipe.ingredientsInRecipe.remove(at: index)
             }
-            if let index = tableContents.firstIndex(
-                where: {$0.self == TableRowContent.component(
-                    name: component.name,
-                    image: component.image,
-                    count: component.amount)}) {
-
-                tableContents.remove(at: index)
-                tableView.deleteRows(at: [componentIndexPath], with: .none)
-            }
-
-            if recipe.ingredientsInRecipe.count == 3 {
-                if let index = tableContents.firstIndex(of: RecipeDetailViewController.TableRowContent.downArrow) {
-                    tableContents.insert(.plusButton(type: .inputNew), at: index)
-                    let indexPath = IndexPath(row: index, section: tableView.numberOfSections - 1)
-                    tableView.insertRows(at: [indexPath], with: .none)
-                }
-            }
-        } else if type == RecipeComponentType.outputChange {
-
+        case .outputChange:
             if let index = recipe.potionsInRecipe.firstIndex(where: {$0.name == component.name}) {
                 recipe.potionsInRecipe.remove(at: index)
             }
-            if let index = tableContents.firstIndex(
-                where: {$0.self == TableRowContent.component(
-                    name: component.name,
-                    image: component.image,
-                    count: component.amount)}) {
+        default:
+            return
+        }
+    }
 
-                tableContents.remove(at: index)
-                tableView.deleteRows(at: [componentIndexPath], with: .none)
-            }
-
-            if recipe.potionsInRecipe.count == 1 {
-                if let index = tableContents.firstIndex(of: RecipeDetailViewController.TableRowContent.makeButton) {
+    func appendPlusButton(type: RecipeComponentType) {
+        switch type {
+        case .inputChange:
+            if recipe.ingredientsInRecipe.count == 3 {
+                if let index = tableContents.firstIndex(
+                    of: RecipeDetailViewController.TableRowContent.downArrow) {
                     tableContents.insert(.plusButton(type: .inputNew), at: index)
                     let indexPath = IndexPath(row: index, section: tableView.numberOfSections - 1)
                     tableView.insertRows(at: [indexPath], with: .none)
                 }
             }
+        case .outputChange:
+            if recipe.potionsInRecipe.count == 1 {
+                if let index = tableContents.firstIndex(
+                    of: RecipeDetailViewController.TableRowContent.makeButton) {
+                    tableContents.insert(.plusButton(type: .inputNew), at: index)
+                    let indexPath = IndexPath(row: index, section: tableView.numberOfSections - 1)
+                    tableView.insertRows(at: [indexPath], with: .none)
+                }
+            }
+        default:
+            return
         }
     }
+
+    // MARK: removeComponent internal functions END
+
 }
